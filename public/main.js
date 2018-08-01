2ba2b3694adf75046a84444283569c8ee3ff0ed9
Alexis Grisham <alexiskjg16@gmail.com>
2018-08-01 15:47:46 -0400

    IDIDATHING

--- public/main.js
+++ public/main.js
@@ -1,5 +1,39 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
+const PlayerShuffledDeck = () => {
+  console.log('player clicked shuffle')
+  // shuffle cards between suits and types
+  // for each card in deck
+  for (let i = 0; i < deck.length; i++) {
+    //create a random number between 0-i
+    const j=Math.floor(Math.random() * i)
+    //swap cards at position i and the random number
+    let temp = deck[i]
+    deck[i] = deck[j]
+    deck[j] = temp
+  }
+  console.log(deck)
 }
 
-document.addEventListener('DOMContentLoaded', main)
+const displayCard = () => {
+  const displayCard = deck.pop()
+  console.log(displayCard)
+}
+
+let deck = []
+
+const createPlayerDeck = () => {
+  let suitOfCard = ['Hearts', 'Spades', 'Diamonds', 'Clubs']
+  let typeOfCard = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
+  for (let j = 0; j < suitOfCard.length; j++) {
+    for (let i = 0; i < typeOfCard.length; i++) {
+      const _card = document.createElement('li')
+      _card.textContent = typeOfCard[i] + " of " + suitOfCard[j];
+      deck.push(typeOfCard[i] + " of " + suitOfCard[j])
+      document.querySelector('.displayCard').appendChild(_card)
+    }
+  }
+  PlayerShuffledDeck()
+}
+
+document.addEventListener('DOMContentLoaded', createPlayerDeck)
+document.querySelector('.LetitGo').addEventListener('click', displayCard)
+document.querySelector('.shuffleButton').addEventListener('click', PlayerShuffledDeck)
